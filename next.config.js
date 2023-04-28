65e1f63a80befe557b72c32f2b54e2582c116850
meryemctnky <meryemctnkya@gmail.com>
2023-04-29 00:15:11 +0300

    Updated files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
